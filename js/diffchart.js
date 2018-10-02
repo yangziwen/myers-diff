2bb69e6718919929f8041e1e4be51387f5800201
yangziwen <yangziwen@baidu.com>
2018-10-02 18:10:09 +0800

    add position chart

--- js/diffchart.js
+++ js/diffchart.js
@@ -1,3 +1,79 @@
+var PositionChart = Object.assign((function(echarts, el) {
+    this.chart = echarts.init(el);
+}).prototype, {
+    refresh(positions) {
+        let originData = positions.map(pos => [pos.x, pos.y]);
+        let prevPos = [0, 0];
+        let data = [prevPos];
+        for (let pos of originData) {
+            let [x, y] = pos;
+            let [x0, y0] = prevPos;
+            let stepX = x - x0;
+            let stepY = y - y0;
+            if (stepX > stepY) {
+                data.push([++x0, y0]);
+            } else if (stepX < stepY) {
+                data.push([x0, ++y0]);
+            }
+            let commonStep = Math.min(stepX, stepY);
+            for (let i = 1; i <= commonStep; i++) {
+                data.push([x0 + i, y0 + i]);
+            }
+            prevPos = pos;
+        }
+        this.chart.setOption({
+            title: {},
+            tooltip: {
+                trigger: 'axis',
+                backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                borderColor: '#ccc',
+                borderWidth: 1,
+                textStyle: {
+                    color: '#666'
+                },
+                formatter(params) {
+                    return params.map(param => {
+                        let [x, y] = param.data;
+                        return `pos: (x=${x}, y=${y})`
+                    }).join('<br/>');;
+                }
+            },
+            animation: false,
+            grid: {
+                left: 35,
+                right: 20,
+                top: 35,
+                bottom: 20
+            },
+            dataZoom: {
+                type: 'inside',
+                zoomOnMouseWheel: 'ctrl'
+            },
+            xAxis: {
+                name: 'x',
+                position: 'top',
+                nameLocation: 'middle',
+                type: 'value',
+                min: 'dataMin',
+                max: 'dataMax'
+            },
+            yAxis: {
+                name: 'y',
+                nameLocation: 'middle',
+                type: 'value',
+                min: 'dataMin',
+                max: 'dataMax',
+                inverse: true
+            },
+            series: [{
+                type: 'line',
+                showSymbol: false,
+                data: data
+            }]
+        }, true);
+    }
+}).constructor;
+
 var StepChart = Object.assign((function(echarts, el, clickCallback) {
     this.chart = echarts.init(el);
     this.chart.on('click', params => clickCallback(params, this.diff));
@@ -65,12 +141,16 @@ var StepChart = Object.assign((function(echarts, el, clickCallback) {
             xAxis: {
                 name: 'd',
                 nameLocation: 'middle',
-                type: 'value'
+                type: 'value',
+                min: 'dataMin',
+                max: 'dataMax'
             },
             yAxis: {
                 name: 'k = x - y',
                 nameLocation: 'middle',
-                type: 'value'
+                type: 'value',
+                min: 'dataMin',
+                max: 'dataMax'
             },
             series: series
         }, true);
